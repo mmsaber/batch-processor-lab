55b53d3288b6b407be4085190061ac259b010fb9
Mohamed <mm.saber91@gmail.com>
2024-01-19 15:11:44 +0300

    first commit

---
+++ src/main/java/org/lab/batchprocessorlab/BatchProcessorLabApplication.java
@@ -0,0 +1,13 @@
+package org.lab.batchprocessorlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProcessorLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchProcessorLabApplication.class, args);
+    }
+
+}
